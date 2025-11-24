f852c862c6c1a51a7858a4fc7820eb5059b020a0
zonary123 <carlosvarasalonso10@gmail.com>
2025-11-24 07:14:17 +0100

    Add SentOutStateMixin to intercept recall method and enhance entity handling

---
+++ src/main/java/org/kingpixel/cobblemonpatches/mixins/things/SentOutStateMixin.java
@@ -0,0 +1,30 @@
+package org.kingpixel.cobblemonpatches.mixins.things;
+
+import com.cobblemon.mod.common.pokemon.activestate.SentOutState;
+import org.kingpixel.cobblemonpatches.CobblemonPatches;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+/**
+ * @author Carlos Varas Alonso - 24/11/2025 6:59
+ */
+@Mixin(value = SentOutState.class, remap = false)
+public abstract class SentOutStateMixin {
+
+  // Interceptar el método recall para añadir lógica antes o después
+  @Inject(method = "recall", at = @At("HEAD"), cancellable = true)
+  private void onRecallHead(CallbackInfo ci) {
+    SentOutState thisState = (SentOutState) (Object) this;
+    CobblemonPatches.server.executeSync(() -> {
+      var entity = thisState.getEntity();
+      if (entity == null) return;
+      entity.discard();
+    });
+    ci.cancel();
+  }
+
+
+}
+
