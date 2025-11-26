10737b497fb9346526e7cee89945e5f468685860
zonary123 <carlosvarasalonso10@gmail.com>
2025-11-26 07:59:07 +0100

    feat: Add SpeciesMixin for showdownId caching and optimize PCStorageIterator

--- src/main/java/org/kingpixel/cobblemonpatches/mixins/SpeciesMixin.java
+++ src/main/java/org/kingpixel/cobblemonpatches/mixins/SpeciesMixin.java
@@ -1,20 +1,17 @@
 package org.kingpixel.cobblemonpatches.mixins;
 
-import com.cobblemon.mod.common.pokemon.FormData;
-import com.cobblemon.mod.common.pokemon.Pokemon;
 import com.cobblemon.mod.common.pokemon.Species;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 /**
  * @author Carlos Varas Alonso - 27/10/2025 6:05
  */
-@Mixin(value = Pokemon.class, remap = false)
-public abstract class PokemonMixin {
+@Mixin(value = Species.class, remap = false)
+public abstract class SpeciesMixin {
 
   // Improve performance of showdownId() by caching the result
   @Unique private String showdownIdCache = null;
@@ -29,15 +26,5 @@ public abstract class PokemonMixin {
   @Inject(method = "showdownId", at = @At("RETURN"))
   private void PokemonMixin$ReturnShowdownId(CallbackInfoReturnable<String> cir) {
     if (showdownIdCache == null) showdownIdCache = cir.getReturnValue().intern();
-  }
-
-  @Inject(method = "setSpecies", at = @At("HEAD"))
-  private void PokemonMixin$setSpecies(Species value, CallbackInfo ci) {
-    showdownIdCache = null;
-  }
-
-  @Inject(method = "setForm", at = @At("HEAD"))
-  private void PokemonMixin$setForm(FormData value, CallbackInfo ci) {
-    showdownIdCache = null;
   }
 }
--- src/main/java/org/kingpixel/cobblemonpatches/mixins/storage/PCStorageMixin.java
+++ src/main/java/org/kingpixel/cobblemonpatches/mixins/storage/PCStorageMixin.java
@@ -19,32 +19,44 @@ import java.util.NoSuchElementException;
 @Mixin(value = PCStore.class, remap = false)
 abstract class PCStorageMixin extends PokemonStore<PCPosition> {
 
-  @Shadow
-  @Final
+  @Shadow @Final
   private List<PCBox> boxes;
 
+  private static final Iterator<Pokemon> EMPTY = Collections.emptyIterator();
 
   @Overwrite
   @NotNull
   @Override
   public Iterator<Pokemon> iterator() {
-    return new Iterator<>() {
-      private final Iterator<PCBox> boxIt = boxes.iterator();
-      private Iterator<Pokemon> currentBoxIt = Collections.emptyIterator();
+    return new PCIterator(boxes);
+  }
 
-      @Override
-      public boolean hasNext() {
-        while (!currentBoxIt.hasNext() && boxIt.hasNext()) {
-          currentBoxIt = boxIt.next().iterator();
-        }
-        return currentBoxIt.hasNext();
-      }
+  private static final class PCIterator implements Iterator<Pokemon> {
 
-      @Override
-      public Pokemon next() {
-        if (!hasNext()) throw new NoSuchElementException();
-        return currentBoxIt.next();
+    private final Iterator<PCBox> boxIterator;
+    private Iterator<Pokemon> pokemonIterator;
+
+    PCIterator(List<PCBox> boxes) {
+      this.boxIterator = boxes.iterator();
+      this.pokemonIterator = EMPTY;
+    }
+
+    @Override
+    public boolean hasNext() {
+      if (pokemonIterator.hasNext()) return true;
+
+      while (boxIterator.hasNext()) {
+        pokemonIterator = boxIterator.next().iterator();
+        if (pokemonIterator.hasNext()) return true;
       }
-    };
+      return false;
+    }
+
+    @Override
+    public Pokemon next() {
+      if (!hasNext()) throw new NoSuchElementException();
+      return pokemonIterator.next();
+    }
   }
 }
+
