7999a748fc978822d758eed1c540c6b43f69eaa2
zonary123 <carlosvarasalonso10@gmail.com>
2025-12-04 07:00:52 +0100

    refactor: Simplify PCStorageMixin iterator implementation and enhance readability

--- src/main/java/org/kingpixel/cobblemonpatches/mixins/storage/PCStorageMixin.java
+++ src/main/java/org/kingpixel/cobblemonpatches/mixins/storage/PCStorageMixin.java
@@ -6,10 +6,7 @@ import com.cobblemon.mod.common.api.storage.pc.PCPosition;
 import com.cobblemon.mod.common.api.storage.pc.PCStore;
 import com.cobblemon.mod.common.pokemon.Pokemon;
 import org.jetbrains.annotations.NotNull;
-import org.spongepowered.asm.mixin.Final;
-import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Overwrite;
-import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.*;
 
 import java.util.Collections;
 import java.util.Iterator;
@@ -17,46 +14,42 @@ import java.util.List;
 import java.util.NoSuchElementException;
 
 @Mixin(value = PCStore.class, remap = false)
-abstract class PCStorageMixin extends PokemonStore<PCPosition> {
+public abstract class PCStorageMixin extends PokemonStore<PCPosition> {
 
   @Shadow @Final
   private List<PCBox> boxes;
 
-  private static final Iterator<Pokemon> EMPTY = Collections.emptyIterator();
+  @Unique private static final Iterator<Pokemon> EMPTY = Collections.emptyIterator();
 
+  /**
+   * @author
+   * @reason Iterate through all boxes to get all Pokemons in the PCStore
+   */
   @Overwrite
   @NotNull
   @Override
   public Iterator<Pokemon> iterator() {
-    return new PCIterator(boxes);
-  }
+    return new Iterator<>() {
+      private final Iterator<PCBox> boxIterator = boxes.iterator();
+      private Iterator<Pokemon> pokemonIterator = EMPTY;
 
-  private static final class PCIterator implements Iterator<Pokemon> {
-
-    private final Iterator<PCBox> boxIterator;
-    private Iterator<Pokemon> pokemonIterator;
-
-    PCIterator(List<PCBox> boxes) {
-      this.boxIterator = boxes.iterator();
-      this.pokemonIterator = EMPTY;
-    }
-
-    @Override
-    public boolean hasNext() {
-      if (pokemonIterator.hasNext()) return true;
-
-      while (boxIterator.hasNext()) {
-        pokemonIterator = boxIterator.next().iterator();
+      @Override
+      public boolean hasNext() {
         if (pokemonIterator.hasNext()) return true;
-      }
-      return false;
-    }
 
-    @Override
-    public Pokemon next() {
-      if (!hasNext()) throw new NoSuchElementException();
-      return pokemonIterator.next();
-    }
+        while (boxIterator.hasNext()) {
+          pokemonIterator = boxIterator.next().iterator();
+          if (pokemonIterator.hasNext()) return true;
+        }
+        return false;
+      }
+
+      @Override
+      public Pokemon next() {
+        if (!hasNext()) throw new NoSuchElementException();
+        return pokemonIterator.next();
+      }
+    };
   }
 }
 
