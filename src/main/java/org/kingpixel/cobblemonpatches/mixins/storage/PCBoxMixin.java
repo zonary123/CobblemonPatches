ff60d5cf6cb1ac81e60dc2f7bb0cddeee8dce922
zonary123 <carlosvarasalonso10@gmail.com>
2025-12-04 07:04:54 +0100

    refactor: Add documentation comments to PCBoxMixin for clarity

--- src/main/java/org/kingpixel/cobblemonpatches/mixins/storage/PCBoxMixin.java
+++ src/main/java/org/kingpixel/cobblemonpatches/mixins/storage/PCBoxMixin.java
@@ -19,6 +19,10 @@ public abstract class PCBoxMixin implements Iterable<Pokemon> {
   @Final
   private Pokemon[] pokemon;
 
+  /**
+   * @author
+   * @reason
+   */
   @Overwrite
   @Override
   @NotNull
