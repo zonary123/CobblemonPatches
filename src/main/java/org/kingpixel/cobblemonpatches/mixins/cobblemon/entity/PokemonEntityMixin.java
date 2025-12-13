1964a15afb6667c4cdd7ec87cccd9cb2570d56a9
MemencioPerez <memencio.perez.2004@gmail.com>
2025-12-13 19:21:44 -0400

    Remove unnecessary condition in PokemonEntity#shouldSave method and avoid calling the Entity#remove method in the PokemonEntity#onStoppedTrackingBy method

--- src/main/java/org/kingpixel/cobblemonpatches/mixins/cobblemon/entity/PokemonEntityMixin.java
+++ src/main/java/org/kingpixel/cobblemonpatches/mixins/cobblemon/entity/PokemonEntityMixin.java
@@ -1,24 +1,38 @@
 package org.kingpixel.cobblemonpatches.mixins.cobblemon.entity;
 
+import com.cobblemon.mod.common.Cobblemon;
 import com.cobblemon.mod.common.CobblemonMemories;
+import com.cobblemon.mod.common.api.events.CobblemonEvents;
+import com.cobblemon.mod.common.api.events.entity.PokemonEntitySaveToWorldEvent;
+import com.cobblemon.mod.common.block.entity.PokemonPastureBlockEntity;
 import com.cobblemon.mod.common.entity.pokemon.PokemonEntity;
 import com.cobblemon.mod.common.pokemon.Pokemon;
-import com.llamalad7.mixinextras.injector.wrapmethod.WrapMethod;
+import com.cobblemon.mod.common.pokemon.activestate.ActivePokemonState;
+import com.cobblemon.mod.common.pokemon.activestate.InactivePokemonState;
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerTickEvents;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.ai.brain.MemoryModuleState;
 import net.minecraft.entity.passive.TameableShoulderEntity;
-import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.world.World;
-import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Overwrite;
-import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.*;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.util.ArrayDeque;
+import java.util.Queue;
 
 @Mixin(PokemonEntity.class)
 public abstract class PokemonEntityMixin extends TameableShoulderEntity {
 
-  @Shadow
-  private Pokemon pokemon;
+  // @formatter:off
+  @Unique private static Queue<PokemonEntity> DESPAWN_QUEUE;
+
+  @Shadow private Pokemon pokemon;
+  @Shadow private PokemonPastureBlockEntity.Tethering tethering;
+  // @formatter:on
 
   protected PokemonEntityMixin(EntityType<? extends TameableShoulderEntity> entityType, World world) {
     super(entityType, world);
@@ -39,15 +53,51 @@ public abstract class PokemonEntityMixin extends TameableShoulderEntity {
       || this.brain.isMemoryInState(CobblemonMemories.NEARBY_SACC_LEAVES, MemoryModuleState.VALUE_PRESENT)));
   }
 
+  @Inject(method = "<clinit>", at = @At("TAIL"))
+  private static void registerEndWorldTickListener(CallbackInfo ci) {
+    DESPAWN_QUEUE = new ArrayDeque<>();
+    ServerTickEvents.END_WORLD_TICK.register(world -> {
+      while (!DESPAWN_QUEUE.isEmpty()) {
+        var pokemonEntity = DESPAWN_QUEUE.remove();
+        if (!pokemonEntity.isRemoved()) {
+          pokemonEntity.discard();
+          continue;
+        }
+
+        var pokemon = pokemonEntity.getPokemon();
+        if (pokemon.getState() instanceof ActivePokemonState) {
+          pokemon.setState(new InactivePokemonState());
+        }
+      }
+    });
+  }
+
   /**
    * @author MemencioPerez
-   * @reason This method will still be called if the condition to call the recall
-   * method in a Pokémon if it moves too far from its corresponding pasture block
-   * passes, trying to remove the entity twice
+   * @reason This method shouldn't be calling Entity#remove, it can cause a
+   * ConcurrentModificationException under specific conditions. We're going
+   * to add the PokemonEntity to a queue that will be processed later in the
+   * Fabric END_WORLD_TICK event listener
    */
-  @WrapMethod(method = "onStoppedTrackingBy")
-  public void doNotCallEntityRemoveTwiceForOwnedPokemon(ServerPlayerEntity player, Operation<Void> original) {
-    if (this.isRemoved()) return;
-    original.call(player);
+  @WrapOperation(method = "onStoppedTrackingBy", at = @At(value = "INVOKE", target = "Lcom/cobblemon/mod/common/entity/pokemon/PokemonEntity;remove(Lnet/minecraft/entity/Entity$RemovalReason;)V"))
+  public void cobblemonpatches$doNotCallEntityRemove(PokemonEntity pokemonEntity, RemovalReason reason, Operation<Void> original) {
+    if (!this.isRemoved()) DESPAWN_QUEUE.add(pokemonEntity);
+  }
+
+  /**
+   * @author MemencioPerez
+   * @reason Remove unnecessary condition in PokemonEntity#shouldSave method
+   */
+  @Overwrite
+  public boolean shouldSave() {
+    if (this.getOwnerUuid() == null
+      && !this.pokemon.isNPCOwned()
+      && (Cobblemon.INSTANCE.getConfig().getSavePokemonToWorld() || this.isPersistent())) {
+      var event = new PokemonEntitySaveToWorldEvent((PokemonEntity) (Object) this);
+      CobblemonEvents.POKEMON_ENTITY_SAVE_TO_WORLD.emit(event);
+      if (!event.isCanceled()) return true;
+    }
+
+    return this.tethering != null;
   }
 }
