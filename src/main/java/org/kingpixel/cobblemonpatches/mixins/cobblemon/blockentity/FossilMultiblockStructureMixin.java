85d290032afac2536fe80c6767ad0761517092d3
Luis <luismartinvives14@gmail.com>
2026-01-28 17:16:10 +0100

    Update FossilMultiblockStructureMixin.java

--- src/main/java/org/kingpixel/cobblemonpatches/mixins/cobblemon/blockentity/FossilMultiblockStructureMixin.java
+++ src/main/java/org/kingpixel/cobblemonpatches/mixins/cobblemon/blockentity/FossilMultiblockStructureMixin.java
@@ -1,15 +1,18 @@
 package org.kingpixel.cobblemonpatches.mixins.cobblemon.blockentity;
 
-import com.cobblemon.mod.common.block.multiblock.FossilMultiblockStructure;
-import com.mojang.serialization.Codec;
-import com.mojang.serialization.DataResult;
-import com.mojang.serialization.DynamicOps;
-import net.minecraft.item.ItemStack;
-import net.minecraft.nbt.NbtElement;
 import org.kingpixel.cobblemonpatches.OpsUtil;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
+
+import com.cobblemon.mod.common.block.multiblock.FossilMultiblockStructure;
+import com.mojang.serialization.Codec;
+import com.mojang.serialization.DataResult;
+import com.mojang.serialization.DynamicOps;
+
+import net.minecraft.component.DataComponentTypes;
+import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NbtElement;
 
 @Mixin(FossilMultiblockStructure.class)
 public abstract class FossilMultiblockStructureMixin {
@@ -26,9 +29,16 @@ public abstract class FossilMultiblockStructureMixin {
     DynamicOps<?> ops,
     Object value
   ) {
+    // FIXING BUG: This new encoder is causing server desync issues. Strip enchantment components in-place.
+    ItemStack stack = (ItemStack) value;
+
+    // Remove NBT enchantments from the item stored in the machine
+    stack.remove(DataComponentTypes.ENCHANTMENTS);
+    stack.remove(DataComponentTypes.STORED_ENCHANTMENTS);
+
     return ItemStack.CODEC.encodeStart(
       OpsUtil.getOps(),
-      (ItemStack) value
+      stack
     );
   }
 }
