e4207439d786104f9890450197651b3992031774
zonary123 <carlosvarasalonso10@gmail.com>
2026-02-23 15:22:11 +0100

    fix: Optimize distance check in GildedChestBlockEntityMixin by using a constant for maximum distance

--- src/main/java/org/kingpixel/cobblemonpatches/mixins/cobblemon/dupe/GildedChestBlockEntityMixin.java
+++ src/main/java/org/kingpixel/cobblemonpatches/mixins/cobblemon/dupe/GildedChestBlockEntityMixin.java
@@ -6,26 +6,27 @@ import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.util.math.BlockPos;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 
 
 @Mixin(value = GildedChestBlockEntity.class, remap = false)
 public abstract class GildedChestBlockEntityMixin {
 
+  @Unique
+  private static final double MAX_DISTANCE_SQ = 8.0D * 8.0D;
+
   @WrapMethod(method = "canPlayerUse")
   public boolean canPlayerUse(PlayerEntity player, Operation<Boolean> original) {
-    double maxDistance = 8.0D;
-    double maxDistanceSq = maxDistance * maxDistance;
-
     GildedChestBlockEntity instance = ((GildedChestBlockEntity) (Object) this);
 
     BlockPos pos = instance.getPos();
+
     double distanceSq = player.squaredDistanceTo(
       pos.getX() + 0.5,
       pos.getY() + 0.5,
       pos.getZ() + 0.5
     );
-
-    return distanceSq <= maxDistanceSq;
+    return distanceSq <= MAX_DISTANCE_SQ;
   }
 
 }
